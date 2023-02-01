db2e8df0f2b8f34dd137ef3078d7f71d1a739825
Sibindra <timalsinasibindra@gmail.com>
2023-02-01 19:00:01 +0545

    added move prop

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,19 @@ import { useState } from "react";
 import Header from "./components/Header";
 import Board from "./components/Board";
 
-
 import "./index.css";
 
 function App() {
+    const [player, setPlayer] = useState("X");
 
-  const [player, setPlayer] = useState('X')
-
-  function changePlayer(){
-    setPlayer(
-      prevPlayer => prevPlayer === 'X' ? 'O' :'X'
-    )
-  }
+    function changePlayer() {
+        setPlayer((prevPlayer) => (prevPlayer === "X" ? "O" : "X"));
+    }
 
     return (
         <div className="App">
-            <Header player={player}/>
-            <Board function={changePlayer}/>
+            <Header player={player} />
+            <Board function={changePlayer} move={player} />
         </div>
     );
 }
--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -1,18 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Board(props) {
     return (
         <>
             <div className="Board">
-                <div className="board-item" onClick={props.function}></div>
-                <div className="board-item" onClick={props.function}></div>
-                <div className="board-item" onClick={props.function}></div>
-                <div className="board-item" onClick={props.function}></div>
-                <div className="board-item" onClick={props.function}></div>
-                <div className="board-item" onClick={props.function}></div>
-                <div className="board-item" onClick={props.function}></div>
-                <div className="board-item" onClick={props.function}></div>
-                <div className="board-item" onClick={props.function}></div>
+                <div className="board-item" onClick={props.function}>{props.move}</div>
+                <div className="board-item" onClick={props.function}>{props.move}</div>
+                <div className="board-item" onClick={props.function}>{props.move}</div>
+                <div className="board-item" onClick={props.function}>{props.move}</div>
+                <div className="board-item" onClick={props.function}>{props.move}</div>
+                <div className="board-item" onClick={props.function}>{props.move}</div>
+                <div className="board-item" onClick={props.function}>{props.move}</div>
+                <div className="board-item" onClick={props.function}>{props.move}</div>
+                <div className="board-item" onClick={props.function}>{props.move}</div>
             </div>
         </>
     );
